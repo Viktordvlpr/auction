0f62e11dac5eb110be4c1a66522e26d53b47c9ed
ArsenHrynevych <102324797+Arsen-hrynevych@users.noreply.github.com>
2024-02-11 16:12:19 +0100

    Added decorator for check session

--- auctionsonline/website/validation.py
+++ auctionsonline/website/validation.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+
+from functools import wraps
 
 def validate_login(username, password):
     user = authenticate(username=username, password=password)
     if user is not None:
-        return True
+        return user
     else:
         return False
 
@@ -22,3 +25,12 @@ def validate_registration(username, password1, password2, email):
         return False
 
     return True
+
+def session_check(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if 'user_id' in request.session and request.session['user_id']:
+            return view_func(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect('/website/')
+    return _wrapped_view
